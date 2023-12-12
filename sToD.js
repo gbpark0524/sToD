615341077aaf2544d5ad80bb262a7a136e1fd035
gbPark <69848531+gbpark0524@users.noreply.github.com>
2023-12-12 18:59:10 +0900

    starting git repository
    starting git repository

---
+++ sToD.js
@@ -0,0 +1,111 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.SToD = factory());
+}(this, (function () {
+    'use strict';
+
+    const NodeType = {
+        ELEMENT_NODE: 1,
+        ATTRIBUTE_NODE: 2,
+        TEXT_NODE: 3,
+        CDATA_SECTION_NODE: 4,
+        ENTITY_REFERENCE_NODE: 5,
+        ENTITY_NODE: 6,
+        PROCESSING_INSTRUCTION_NODE: 7,
+        COMMENT_NODE: 8,
+        DOCUMENT_NODE: 9,
+        DOCUMENT_TYPE_NODE: 10,
+        DOCUMENT_FRAGMENT_NODE: 11,
+        NOTATION_NODE: 12
+    };
+    Object.freeze(NodeType);
+
+    function convertHTMLToJS(htmlString, options) {
+        const varSet = new Set();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+        const elements = doc.body.childNodes;
+        const defaultOptions = {
+            useId: true,
+            classAdd: true,
+        }
+        if (!options) options = defaultOptions;
+        const stack = [];
+        let jsCode = '';
+
+        const getVName = (tag) => {
+            var count = 1;
+            while (count < 1000) {
+                if (!varSet.has(tag + count)) {
+                    varSet.add(tag + count);
+                    return tag + count;
+                }
+                count++;
+            }
+            if (count === 1000) {
+                document.querySelector("#result").innerText = "so many same tag names";
+            }
+        }
+
+        const appendChildNodes = (element, vName) => {
+            element.childNodes.forEach((childNode, index) => {
+                if (childNode.nodeType === NodeType.TEXT_NODE) {
+                    let nodeText = `${childNode.textContent.trim()}`;
+                    if (!nodeText) return;
+                    jsCode += `textNode = document.createTextNode(\`${nodeText}\`);\n`;
+                    jsCode += `${vName}.appendChild(textNode);\n`;
+                } else {
+                    const childTag = defaultOptions.useId && !!childNode.id ? childNode.id : `${childNode.tagName.toLowerCase()}`;
+                    const cvName = getVName(childTag);
+                    jsCode += createJSForElement(childNode, cvName);
+                    jsCode += `${vName}.appendChild(${cvName});\n`;
+                    appendChildNodes(childNode, cvName);
+                }
+            });
+        }
+
+        const createJSForElement = (element, varName) => {
+            let js = `const ${varName} = document.createElement('${element.tagName.toLowerCase()}');\n`;
+            if (element.id) {
+                js += `${varName}.id = '${element.id}';\n`;
+                element.removeAttribute('id');
+            }
+            if (element.className) {
+                if (options.classAdd === true) {
+                    element.classList.forEach((className) => {
+                        js += `${varName}.classList.add('${element.className}');\n`;
+                    });
+                } else {
+                    js += `${varName}.className = '${element.className}';\n`;
+                }
+                element.removeAttribute('class');
+            }
+            let attributeNames = element.getAttributeNames();
+            attributeNames.forEach((attributeName) => {
+                js += `${varName}.setAttribute('${attributeName}', \`${element.getAttribute(attributeName)}\`);\n`;
+            });
+            return js;
+        };
+
+        elements.forEach((element) => {
+            const vName = getVName(defaultOptions.useId && !!element.id ? element.id : `${element.tagName.toLowerCase()}`);
+            jsCode += createJSForElement(element, vName);
+            if (!vName) return;
+
+            appendChildNodes(element, vName);
+        });
+
+        return jsCode;
+    }
+
+    (function () {
+        document.querySelector("#convertButton").addEventListener("click", function () {
+            try {
+                document.querySelector("#result").innerText = convertHTMLToJS(document.querySelector("#inputText").value);
+            } catch (e) {
+                document.querySelector("#result").innerText = e.stack;
+            }
+        });
+    }());
+})));
